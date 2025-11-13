4303190ec814083ded74597ea6055db31013132e
Junseung-Ock <junseung0305@kyonggi.ac.kr>
2025-11-13 20:16:18 +0900

    gitReader

---
+++ src/main/java/com/example/skillboost/githubReader/GithubFile.java
@@ -0,0 +1,16 @@
+package com.example.skillboost.githubReader;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GithubFile {
+    private String path;
+    private String content;
+
+    public GithubFile(String path, String content) {
+        this.path = path;
+        this.content = content;
+    }
+}
