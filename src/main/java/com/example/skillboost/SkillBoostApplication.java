6f256a535f9597a5b24cfc0ee1b69304378f30a0
yeongbinbae <yeongbinbae01@gmail.com>
2025-11-13 16:04:33 +0900

    first commit

---
+++ src/main/java/com/example/skillboost/SkillBoostApplication.java
@@ -0,0 +1,13 @@
+package com.example.skillboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillBoostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillBoostApplication.class, args);
+    }
+
+}
