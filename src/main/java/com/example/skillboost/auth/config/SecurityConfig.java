3841f96bbc0c0a4f9670cb7e42bf213a2343a23c
yeongbinbae <yeongbinbae01@gmail.com>
2025-11-30 17:56:40 +0900

    setting: 요청 경로 권한 설정

--- src/main/java/com/example/skillboost/auth/config/SecurityConfig.java
+++ src/main/java/com/example/skillboost/auth/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 // 요청 권한 설정
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/",
                                 "/api/auth/**",
                                 "/oauth2/**",
                                 "/login/oauth2/**",
